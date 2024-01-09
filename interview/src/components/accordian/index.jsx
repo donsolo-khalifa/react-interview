4e50d6ac875c161fb835d0b542746f881a6f33dd
donsolo-khalifa <101540488+donsolo-khalifa@users.noreply.github.com>
2024-01-09 21:59:30 +0100

    completed single accordian

--- interview/src/components/accordian/index.jsx
+++ interview/src/components/accordian/index.jsx
@@ -4,17 +4,23 @@ import data from "./data";
 function Accordian() {
   const [selected, setSelected] = useState(null);
   function handleSingleSelection(id) {
-    return console.log(id);
+    setSelected(id === selected ? null : id);
   }
+  console.log(selected);
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((item) => (
             <div key={item.id} className="item">
-              <div  className="title">
-                <h3 onClick={()=>handleSingleSelection(item.id)}>{item.question}</h3>
+              <div
+                onClick={() => handleSingleSelection(item.id)}
+                className="title"
+              >
+                <h3>{item.question}</h3>
+                <span>-</span>
               </div>
+              {selected === item.id ? <div className="content">{item.answer}</div> : null}
             </div>
           ))
         ) : (
@@ -26,6 +32,3 @@ function Accordian() {
 }
 
 export default Accordian;
-
-
-
